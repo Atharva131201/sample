c5556e6118f9d2f2043088b961ab15e3b050eb82
Atharva Nandurkar <111697619+Atharva131201@users.noreply.github.com>
2024-07-12 10:00:46 +0530

    Update example.sql

--- example.sql
+++ example.sql
@@ -1,1 +1,14 @@
-Select * from table where id=5
+-- Create a table
+CREATE TABLE my_table (
+    id INTEGER PRIMARY KEY,
+    column1 TEXT,
+    column2 INTEGER
+);
+
+-- Insert some sample data
+INSERT INTO my_table (id, column1, column2) VALUES (1, 'Value1', 100);
+INSERT INTO my_table (id, column1, column2) VALUES (2, 'Value2', 200);
+INSERT INTO my_table (id, column1, column2) VALUES (5, 'Value5', 500);
+
+-- Your select query
+SELECT * FROM my_table WHERE id = 5;
